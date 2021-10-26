a02e9e54201c6c5288f8f2c7313c087dbc1f3e67
FlutLab.io <>
2021-10-26 08:14:09 +0000

    Autosave

--- lib/util/propertiesUtil.dart
+++ lib/util/propertiesUtil.dart
@@ -5,15 +5,14 @@ class PropertiesUtil {
     return PropertiesUtil.propertiesMap()[key].toString();
   }
 
-  static LinkedHashMap<String, dynamic> propertiesMap() {
-    Map<String, dynamic> aMap = {
+  static Map propertiesMap() {
+    Map aMap = {
       "app-id": "flutter-template",
       "app-name": "flutter-template 1.0",
       "apiUrl": "https://flutter-backend.imagworkshop.com/wp-json/wp/v2/",
       "app-version": "0.0.1",
       "pageid-initparam": 10
     };
-    LinkedHashMap astr = LinkedHashMap.from(aMap);
-    return astr;
+    return aMap;
   }
 }

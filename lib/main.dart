9b38e1581e27f9f9843a78b2cc16d8be8c6f2752
FlutLab.io <>
2021-10-26 07:44:27 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'testjson.dart';
-import 'package:global_configuration/global_configuration.dart';
-import 'dart:io';
-import 'dart:convert';
 
 void main() async {
-  final configFile = File('dev.json');
-  final jsonString = await configFile.readAsString();
-  final dynamic jsonMap = jsonDecode(jsonString);
-  print(jsonMap);
   runApp(MyApp());
 }
 
--- lib/util/propertiesUtil.dart
+++ lib/util/propertiesUtil.dart
@@ -1,7 +1,16 @@
-import 'package:global_configuration/global_configuration.dart';
-
 class PropertiesUtil {
   static String getProp(String key) {
-    return GlobalConfiguration().getValue(key);
+    return PropertiesUtil.propertiesMap()[key];
+  }
+
+  static Map<String, String> propertiesMap() {
+    Map astr = {
+      "app-id": "flutter-template",
+      "app-name": "flutter-template 1.0",
+      "apiUrl": "https://flutter-backend.imagworkshop.com/wp-json/wp/v2/",
+      "app-version": "0.0.1",
+      "pageid-initparam": 10
+    };
+    return astr;
   }
 }

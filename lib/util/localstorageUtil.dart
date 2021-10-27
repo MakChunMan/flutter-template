7fef858b7231420966ce3d88caf2ddf3ee856a6e
FlutLab.io <>
2021-10-27 04:27:13 +0000

    Autosave

--- lib/util/localstorageUtil.dart
+++ lib/util/localstorageUtil.dart
@@ -1,10 +1,11 @@
 import 'package:localstorage/localstorage.dart';
+import 'dart:convert';
 
 class LocalStorageUtil {
   static final LocalStorage storage = new LocalStorage('lstore.json');
 
   static void saveToStorage(String key, String value) {
-    storage.setItem(key, value);
+    storage.setItem(key, json.encode(value));
   }
 
   static void clearStorage() async {

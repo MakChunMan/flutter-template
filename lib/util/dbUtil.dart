acd1058e8f51891ff7773bf1600ce01944cd0e3c
FlutLab.io <>
2021-10-27 06:31:27 +0000

    Autosave

--- lib/util/dbUtil.dart
+++ lib/util/dbUtil.dart
@@ -1,24 +1,3 @@
-import 'package:sqflite/sqflite.dart';
+import 'package:moor/moor.dart';
 
-class DBUtil {
-  static var db;
-
-  static Future<String> initDB() async {
-    print('initDB() started:' + await getDatabasesPath());
-    db = await openDatabase('localstorage.db', version: 1, onCreate: (Database db, int version) async {
-      await db.execute('''
-          create table LocalStorage(
-            Id integer primary key autoincrement, 
-            Key varchar not null,
-            Value text not null)
-          ''');
-    });
-    return new Future.value("Done");
-  }
-
-  checkDB() {
-    if (db == null) {
-      initDB();
-    }
-  }
-}
+class DBUtil {}

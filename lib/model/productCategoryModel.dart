cf86ea3f7a95e2f0409a2ff4520b9bbe09b4c893
FlutLab.io <>
2021-11-02 06:19:55 +0000

    Autosave

--- lib/model/productCategoryModel.dart
+++ lib/model/productCategoryModel.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'dart:convert';
 
 class ProductCategoryModel {
-  final String id;
+  final int id;
   final String name;
   final String description;
   final String imagelink;
@@ -20,13 +20,30 @@ class ProductCategoryModel {
 
   factory ProductCategoryModel.fromJson(Map<String, dynamic> json) {
     return ProductCategoryModel(
-      id: json['id'].toString() as String,
+      id: json['id'] as int,
       name: json['name'] as String,
       description: json['description'] as String,
       //imagelink: json['image'].src as String,
-      imagelink: jsonDecode(json['image'])['src'] as String,
+      imagelink: (CategoryImage.fromJson(json["image"])).src,
       //catlink: json['_links']['self'][0] as String,
       //catcount: json['count'] as int,
+    );
+  }
+}
+
+class CategoryImage {
+  final int id;
+  final String src;
+
+  CategoryImage({
+    @required this.id,
+    this.src,
+  });
+
+  factory CategoryImage.fromJson(Map<String, dynamic> json) {
+    return CategoryImage(
+      id: json['id'] as int,
+      src: json['src'] as String,
     );
   }
 }

2dd2287a0a9160850c78945d1df26c737aa96fa1
FlutLab.io <>
2021-11-02 07:04:50 +0000

    Autosave

--- lib/page/shopMainPage.dart
+++ lib/page/shopMainPage.dart
@@ -27,6 +27,7 @@ class _ShopMainPageState extends State<ShopMainPage> {
 
   //Page Model
   Future<List<ProductCategoryModel>> categoryListFuture;
+  List<ProductCategoryModel> _categoryList;
 
   @override
   void initState() {
@@ -35,6 +36,7 @@ class _ShopMainPageState extends State<ShopMainPage> {
     categoryListFuture.then((s) {
       setState(() {
         //this.pageTitle = s.title;
+        this._categoryList = s;
       });
     });
   }
@@ -51,7 +53,74 @@ class _ShopMainPageState extends State<ShopMainPage> {
               decoration: InputDecoration(border: OutlineInputBorder(), hintText: 'Enter text to search', labelText: 'Search', prefixIcon: Icon(Icons.search)),
             ),
           ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _categoryListView(),
+                //_buildCardListView(),
+                //_buildRequestListView(),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  SizedBox _categoryListView() {
+    return SizedBox(
+      height: 150.0,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _categoryList.length,
+        itemExtent: 100.0,
+        itemBuilder: (context, index) {
+          var item = _categoryList[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 4.0),
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    item.catlink,
+                  ),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black26,
+                    BlendMode.darken,
+                  ),
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.grey,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          'https://iisy.fi/wp-content/uploads/2018/08/user-profile-male-logo.jpg',
+                        ),
+                        radius: 14.0,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      item.name,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }

89036df24a5012f055a287073c297297a1446210
FlutLab.io <>
2021-11-03 03:06:09 +0000

    Autosave

--- lib/page/mainPage.dart
+++ lib/page/mainPage.dart
@@ -54,6 +54,7 @@ class _MainPageState extends State<MainPage> {
             'Headline',
             style: TextStyle(fontSize: 18),
           ),
+          _Slider2(),
           SizedBox(
             height: 150.0,
             child: _SliderList(),
@@ -83,6 +84,34 @@ class _MainPageState extends State<MainPage> {
         itemBuilder: (BuildContext context, int index) => Card(
               child: Center(child: Text('Dummy Card Text')),
             ));
+  }
+
+  //Main Content Slider version 2
+  int _slideIndex = 0;
+  SizedBox _Slider2() {
+    return SizedBox(
+      height: 200, // card height
+      child: PageView.builder(
+        itemCount: 10,
+        controller: PageController(viewportFraction: 0.7),
+        onPageChanged: (int index) => setState(() => _slideIndex = index),
+        itemBuilder: (_, i) {
+          return Transform.scale(
+            scale: i == _slideIndex ? 1 : 0.9,
+            child: Card(
+              elevation: 6,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+              child: Center(
+                child: Text(
+                  "Card ${i + 1}",
+                  style: TextStyle(fontSize: 32),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 
   //Main Cotent 2: Button Grid

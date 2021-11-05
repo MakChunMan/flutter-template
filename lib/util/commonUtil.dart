dc74acea0fed5dc23991b9411cb316bcc3107167
FlutLab.io <>
2021-11-05 07:35:13 +0000

    Autosave

--- lib/util/commonUtil.dart
+++ lib/util/commonUtil.dart
@@ -1,9 +1,10 @@
 class CommonUtil {
   static Map<String, dynamic> cartFromJson(Map<int, int> cart) {
-    var _returnCart;
+    Map<String, int> _returnCart = new Map();
     cart.forEach((k, v) {
       _returnCart["$k"] = v;
     });
+    print(_returnCart.length.toString());
     return _returnCart;
   }
 }
